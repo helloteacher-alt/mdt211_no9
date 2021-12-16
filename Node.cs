3165b5ede01309f75f9f9e551a7a9847a3074f73
helloteacher-alt <panyisa20012544@gmail.com>
2021-12-16 12:37:28 +0700

    node.cs

---
+++ Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mdt211_no9
+{
+    class Node
+    {
+        public string Name;
+        public Node(string nameValue)
+        {
+            Name = nameValue;
+        }
+    }
+
+    class OtherNameNode : Node
+    {
+        public Node left;
+        public Node right;
+        public OtherNameNode(string nameValue) : base(nameValue)
+        {
+
+        }
+    }
+
+    class NameNodeUpgrade : Node
+    {
+        public NameNodeUpgrade(string nameValue) : base(nameValue)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace mdt211_no9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = null;
+            AddNode(ref root);
+
+            Node ptr = root;
+            Console.WriteLine(ptr.Name);
+
+
+            static void InputSkill()
+            {
+                Console.Write("Input skill name: ");
+                string skillName = Console.ReadLine();
+            }
+        }
+        /*static void AddDependacy()
+        {
+            Console.Write("Add dependacy for{0} ? (Y/N) ");
+        }*/
+
+        static void AddNode(ref Node node)
+        {
+            Console.Write("Input skill name:");
+            string skillName = Console.ReadLine();
+            if (skillName != "?")
+            {
+                Console.Write("Add dependacy for {0}? (Y/N):", skillName);
+                char answer = char.Parse(Console.ReadLine());
+
+                if (answer == 'Y')
+                {
+                    
+                }
+                else if (answer == 'N')
+                {
+                    
+                }
+                node = new OtherNameNode(skillName);
+                AddNode(ref (node as OtherNameNode).left);
+                AddNode(ref (node as OtherNameNode).right);
+            }
+
+            else
+            {
+                int countSkill = 0;
+                while (skillName != "?")
+                {
+                    string skill = Console.ReadLine();
+                    countSkill++;
+                }
+                Console.WriteLine(countSkill);
+            }
+        }
+
+    }
+}
+    
+    
